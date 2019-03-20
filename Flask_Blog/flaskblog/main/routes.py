ee45d8b1ea1a8d8b22a3e1e3f4c4eba3a3a8a3c1
satishkumar11 <kr.satish123@gmail.com>
2019-03-20 21:16:23 +0530

    Second Commit

--- Flask_Blog/flaskblog/main/routes.py
+++ Flask_Blog/flaskblog/main/routes.py
@@ -1,19 +1,27 @@
 from flask import render_template, request, Blueprint
-from flaskblog.models import Post
+from flaskblog.models import Post, Expert
 import requests
 from flaskblog.posts.forms import Market
 from flask import Response
+from flask import current_app
 main = Blueprint('main', __name__)
 
 @main.route("/", methods=['GET', 'POST'])
 @main.route("/home", methods=['GET', 'POST'])
 def home():
-    print("sdkfhgskdfjhs kjdfkjhfsdkjhksjdfh kjf")
+    imageArray = [{'image':'default.jpg', 'name':'A'},{'image':'Satish1.jpg','name':'B'},{'image':'Satish.jpg','name':'C'}]
+
     # posts = Post.query.all()
     page = request.args.get('page', 1, type = int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
 
+    experts = Expert.query.all()
+    # print("experts = ",experts)
+    # experts = experts[0]
+    # print(experts.username, experts.firstname, experts.firstname, experts.email, experts.description)
+
     market = Market()
+    # websiteExperience = CommentYourExperience()
 
     # str = 'https://www.quandl.com/api/v3/datasets/NSE/DVL?start_date=2019-01-04&end_date=2019-01-04&api_key=xB2axeWukJ1qpQiD-Rg5'
     # r = requests.get(str)
@@ -25,16 +33,24 @@ def home():
     json_object = {}
     if market.validate_on_submit():
         print("aaaaa",market)
-        print("Satish =  ",market)
-        print("Show Data1 = ",market.stockname.data)
+        # print("Satish =  ",market)
+        # print("Show Data1 = ",market.stockname.data)
         name = market.stockname.data
         r = requests.get('https://www.quandl.com/api/v3/datasets/NSE/'+name+'?column_index=1&api_key=xB2axeWukJ1qpQiD-Rg5')
         json_object = r.json()
-        print("JSONNN1 = ",json_object)
+        # print("JSONNN1 = ",json_object)
         # return redirect(url_for('home'))
 
     # return render_template('home.html', data=posts)
-    return render_template('home.html', posts=posts, market=market, data=json_object)
+    # print("JSON OBJECT = ",json_object)
+    newsData = getnews()
+    
+    # if websiteExperience.validate_on_submit() and websiteExperience.submitFeedback.data:
+        # name = websiteExperience.experience.data
+        # print("Experience = "+name)
+
+    # print("newsData = ",newsData)
+    return render_template('home.html', posts=posts, market=market, data=json_object, experts=experts, newsData=newsData)
     # return "Hello Satish!"
 
 
@@ -65,3 +81,9 @@ def home():
 @main.route("/about")
 def about():
     return render_template('about.html', title = 'About')
+
+
+def getnews():
+    # print("News Api Key",current_app.config['NEWS_API_KEY'])
+    r = requests.get('https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey='+current_app.config['NEWS_API_KEY'])
+    return r.json()
--- Flask_Blog/flaskblog/models.py
+++ Flask_Blog/flaskblog/models.py
@@ -108,3 +108,19 @@ class PostLike(db.Model):
 
     def __repr__(self):
         return f"Comment('{self.date_posted}')"
+
+class Expert(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    description = db.Column(db.String(120), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    rating = db.Column(db.Integer, nullable=True)
+    state = db.Column(db.CHAR, nullable=True)
+
+    def __repr__(self):
+        return f"Expert('{self.username}', '{self.email}', '{self.image_file}')"
