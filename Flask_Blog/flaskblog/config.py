f731533d2b6803bff8dcbd0cc64bdab67856767d
Satish Kumar <kr.satish123@gmail.com>
2019-04-17 21:13:24 +0530

    Update config.py

--- Flask_Blog/flaskblog/config.py
+++ Flask_Blog/flaskblog/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://nitssats:nitish01@stockmarket.ciddqkpl6tr6.ap-south-1.rds.amazonaws.com:3306/nitssats'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
